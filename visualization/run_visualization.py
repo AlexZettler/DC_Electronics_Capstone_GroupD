f8736caa090d56b5a61b79cb50245e4b84f9135f
Unknown <azettler@live.com>
2019-02-07 14:02:17 -0500

    Visualization Colors!

--- visualization/run_visualization.py
+++ visualization/run_visualization.py
@@ -5,5 +5,8 @@ import sys
 
 app = QtWidgets.QApplication(sys.argv)
 #settings = QtCore.QSettings('Alex Zettler', 'Data visualization')
-visualization = VisApp()
+
+main_color = "#6e7f59"
+
+visualization = VisApp(color=main_color)
 sys.exit(app.exec_())
--- visualization/visualization_app.py
+++ visualization/visualization_app.py
@@ -1,44 +1,55 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QSizePolicy, QPushButton, QAction
-from PyQt5.QtGui import QIcon
-
-import visualization.rand_cmap as rand_cmap
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QSizePolicy, QPushButton, QAction, QTabWidget, QTabBar
+from PyQt5.QtGui import QIcon, QColor
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
+from collections import deque
+
+import datetime
+
 import random
 
 class VisApp(QMainWindow):
-    def __init__(self):
+    def __init__(self, color):
         super().__init__()
         self.title = "visualization!"
         self.left = 10
         self.top = 30
         self.width = 640
         self.height = 480
+
+        self.primary_color = color
+        self.dark_color = QColor(color).darker(125).name()
+        self.light_color = QColor(color).lighter(125).name()
+
+        print(self.primary_color, self.light_color, self.dark_color)
+
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
+
+        #Setup color
+        self.setStyleSheet(f"background-color: {self.primary_color}")
+
+        # Set positions
         self.setGeometry(self.left, self.top, self.width, self.height)
+
         self.statusBar().showMessage("statusbar thing")
 
-        m = PlotCanvas(self, width=5,height=4, dpi=100)
+        m = PlotCanvas(self, width=5, height=4, dpi=100, color=self.light_color)
         m.move(0,0)
 
-        button = QPushButton("A button", self)
+        button = QPushButton("Reload data!", self)
         button.setToolTip("I'm a button!")
         button.clicked.connect(m.plot_temperatures)
+        button.setStyleSheet(f"background-color: {self.light_color}")
         button.move(500,0)
         button.resize(140,100)
 
         self.show()
-
-
-
-    def initTriggers(self):
-        pass
 
 
     def wasClicked(self):
@@ -46,10 +57,26 @@ class VisApp(QMainWindow):
 
 
 class PlotCanvas(FigureCanvasQTAgg):
-    def __init__(self, parent, width, height, dpi):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
+
+    color_options = {
+        "C0",
+        "C1",
+        "C2",
+        "C3",
+        "C4",
+        "C5",
+        "C6",
+        "C7",
+        "C8",
+        "C9",
+    }
+
+    def __init__(self, parent, width, height, dpi, color):
+        fig = Figure(figsize=(width, height), dpi=dpi, facecolor=color)
+        self.axes = fig.add_subplot(111, facecolor=color)
         self.axes.set_title("LOL a title")
+
+        #self.axes.set_facecolor = color
 
         FigureCanvasQTAgg.__init__(self, fig)
         self.setParent(parent)
@@ -64,26 +91,28 @@ class PlotCanvas(FigureCanvasQTAgg):
         #self.
 
 
-    def plot(self, data, color='purple'):
+    def plot(self, data, color):
         #ax = self.figure.add_subplot(111)
-        self.axes.plot(data, 'r-',color=color)
+        self.axes.plot(data, color=color)
 
         self.draw()
 
 
     def plot_temperatures(self):
         self.axes.clear()
-
         data = self.get_data()
-        colors = self.get_cmap(len(data))
-        print(data, colors)
-        #for d,c in zip(data, colors):
-            #print(d,c)
-        self.plot(data)
 
+        #Custom color cycling
+        color_deque = deque(self.color_options)
 
-    #rand_cmap(100, type='bright', first_color_black=True, last_color_black=False, verbose=True)
+        self.axes.set_title(f"Data({datetime.datetime.now().strftime('%H:%M:%S')})")
+        for d in data:
+
+            self.plot(d, color_deque[0])
+
+            color_deque.rotate(1)
+
 
 
     def get_data(self):
-        return [random.random() for i in range(25)]
+        return [[random.random() for i in range(25)]for i in range(5)]
