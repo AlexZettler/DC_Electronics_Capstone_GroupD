0610584c7f962b995e888247681825d7a8dcb07a
Unknown <azettler@live.com>
2019-02-07 18:32:19 -0500

    Logging more standardized

--- custom_logger.py
+++ custom_logger.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import os.path as path
 
 csv_formatter = logging.Formatter(
     fmt="%(asctime)s,%(levelname)s,%{message}s,",
@@ -14,7 +16,10 @@ def create_measurement_logger(device_id):
     logger = logging.getLogger(f"measurement-{device_id}")
     logger.setLevel(logging.INFO)
 
-    f_handler = logging.FileHandler(f"./log/measurements/{device_id}.csv")
+    file_name = f"./log/measurements/{device_id}.csv"
+    create_path_for_file(file_name)
+
+    f_handler = logging.FileHandler(file_name)
     f_handler.setFormatter(csv_formatter)
     logger.addHandler(f_handler)
 
@@ -29,7 +34,10 @@ def create_output_logger(device_id):
     logger = logging.getLogger(f"output-{device_id}")
     logger.setLevel(logging.INFO)
 
-    f_handler = logging.FileHandler(f"./log/outputs/{device_id}.csv")
+    file_name = f"./log/outputs/{device_id}.csv"
+    create_path_for_file(file_name)
+
+    f_handler = logging.FileHandler(file_name)
     f_handler.setFormatter(csv_formatter)
     logger.addHandler(f_handler)
 
@@ -44,7 +52,10 @@ def create_system_logger():
     logger = logging.getLogger("system")
     logger.setLevel(logging.INFO)
 
-    f_handler = logging.FileHandler("./log/system.csv")
+    file_name = "./log/system.csv"
+    create_path_for_file(file_name)
+
+    f_handler = logging.FileHandler(file_name)
     f_handler.setFormatter(csv_formatter)
     logger.addHandler(f_handler)
 
@@ -53,3 +64,14 @@ def create_system_logger():
     logger.addHandler(s_handler)
 
     return logger
+
+
+def create_path_for_file(file):
+    file_path = os.path.basename(file)
+    dir_path = path.dirname(file_path)
+    print(dir_path)
+
+    if not path.exists(dir_path):
+        os.makedirs(dir_path)
+
+create_path_for_file("./lol/lol.txt")
