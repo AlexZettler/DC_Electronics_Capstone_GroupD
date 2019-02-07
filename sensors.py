d9e40357677d6aed0de2eb9ac9a7b22846b1e5e1
Unknown <azettler@live.com>
2019-02-07 13:12:19 -0500

    Individualized temperature loggers

--- sensors.py
+++ sensors.py
@@ -2,21 +2,21 @@ from data_classes import Temperature, TemperatureReading
 import custom_logger
 from custom_errors import OverTemperature, UnderTemperature
 
-temp_logger = custom_logger.create_measurement_logger()
+system_logger = custom_logger.create_system_logger()
 
 class TemperatureSensor(object):
     def __init__(self, _id):
         self.id = _id
-        raise NotImplementedError
+        self.logger = custom_logger.create_measurement_logger(_id)
 
     def get_temperature(self)->Temperature:
+        #todo: get the temperature reading
         temp_reading = None
         self.log_temperature(temp_reading)
-        raise NotImplementedError
         return temp_reading
 
     def log_temperature(self, temp_reading):
-        temp_logger.info(msg=f"{self.id},{temp_reading}")
+        self.logger.info(msg=f"{self.id},{temp_reading}")
 
 class TargetTemperatureSensor(TemperatureSensor):
     def __init__(self, _id, target_temperature: Temperature):
@@ -35,14 +35,10 @@ class ElementMonitor(TemperatureSensor):
 
     def check_temperature_limits(self, current_temp):
         if current_temp > self.max_temp:
-            self.log_temperature_extreme(current_temp)
+            system_logger.critical(f"Element turning off! {current_temp} is above it's limit of {self.max_temp} ")
             raise OverTemperature
 
         if current_temp < self.min_temp:
-            self.log_temperature_extreme(current_temp)
+            system_logger.critical(f"Element turning off! {current_temp} is below it's limit of {self.min_temp} ")
             raise UnderTemperature
-
-    def log_temperature_extreme(self, current_temp):
-        pass
-
 
