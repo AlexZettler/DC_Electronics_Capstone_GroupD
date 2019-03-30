c089ff6a00af67bd5517cb4da59b16552de6d35b
Unknown <azettler@live.com>
2019-03-29 21:15:36 -0400

    Order of operations maybe

--- system/main_system.py
+++ system/main_system.py
@@ -376,11 +376,11 @@ class System(object):
             if self.element.enabled:
 
                 # If system in heating mode and the room temperature is still below the target
-                if self.element.heating and room_error_readings[_id].celsius >= 0.0:
+                if self.element.heating and (room_error_readings[_id].celsius >= 0.0):
                     servo.rotate_to_angle(90.0)
 
                 # If system in cooling mode and the room temperature is still above the target
-                elif self.element.cooling and room_error_readings[_id].celsius <= 0.0:
+                elif self.element.cooling and (room_error_readings[_id].celsius <= 0.0):
                     servo.rotate_to_angle(90.0)
 
                 # The case if the temperature target has not been reached given the current system state
